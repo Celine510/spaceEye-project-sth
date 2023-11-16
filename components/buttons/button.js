037184a00e4bab29273262744d92ad4e5fe9dbb6
Celine <celinewu1010@gmail.com>
2023-11-16 21:27:29 +0800

    adjust buttons

--- components/buttons/button.js
+++ components/buttons/button.js
@@ -1,38 +1,36 @@
 const btnMail = document.querySelectorAll('.btn-mail');
-const svg = document.querySelectorAll('.btn-mail-svg');
+
+const svgM = document.querySelector('.btn-mail-m-svg');
+const svgL = document.querySelector('.btn-mail-l-svg');
 const btnUpL = document.querySelector('.btn-up-l');
 const btnUpM = document.querySelector('.btn-up-m');
 const btnUpSvg = document.querySelectorAll('.btn-up-svg');
 
+console.log(btnMail[0].childNodes[5].children[1]);
 /* 按鈕 - 歡迎免費諮詢 */
-btnMail.forEach(item => {
-  item.addEventListener('mouseover',e=>{
-    svg.forEach(item => {
-      item.innerHTML = `
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <mask id="mask0_2918_2430" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-        <rect width="24" height="24" fill="#D9D9D9"/>
-        </mask>
-        <g mask="url(#mask0_2918_2430)">
-        <path d="M16.175 13H4V11H16.175L10.575 5.4L12 4L20 12L12 20L10.575 18.6L16.175 13Z" fill="#1E1E1E"/>
-        </g>
-      </svg>
-      `
-    })
+for(let i = 0;i < btnMail.length; i++){
+  btnMail[i].addEventListener('mouseover', e => {
+    btnMail[i].childNodes[5].children[1].innerHTML = `
+    <mask id="mask0_2918_2430" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+    <rect width="24" height="24" fill="#D9D9D9"/>
+    </mask>
+    <g mask="url(#mask0_2918_2430)">
+    <path d="M16.175 13H4V11H16.175L10.575 5.4L12 4L20 12L12 20L10.575 18.6L16.175 13Z" fill="#1E1E1E"/>
+    </g>
+    `
   })
-  item.addEventListener('mouseout', e => {
-    svg.forEach(item => {
-      item.innerHTML = `
+  btnMail[i].addEventListener('mouseout', e => {
+    btnMail[i].childNodes[5].children[1].innerHTML = `
       <mask id="mask0_2910_7237" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-          <rect width="24" height="24" fill="#D9D9D9" />
-        </mask>
-        <g mask="url(#mask0_2910_7237)">
-          <path d="M16.175 13H4V11H16.175L10.575 5.4L12 4L20 12L12 20L10.575 18.6L16.175 13Z" fill="white"/>
-        </g>
-      `
-    })
+        <rect width="24" height="24" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_2910_7237)">
+        <path d="M16.175 13H4V11H16.175L10.575 5.4L12 4L20 12L12 20L10.575 18.6L16.175 13Z" fill="white" />
+      </g>
+    `
   })
-});
+}
+
 
 /* 按鈕 - 向上箭頭 */
 btnUpL.addEventListener('mouseover',e=>{
